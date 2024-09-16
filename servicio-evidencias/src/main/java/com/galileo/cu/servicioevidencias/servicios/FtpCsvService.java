3fd13b62efc078902221b82fd018a5dbd495cd20
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-16 10:59:13 -0400

    2-new Methods FTP && remove olds methods

--- servicio-evidencias/src/main/java/com/galileo/cu/servicioevidencias/servicios/FtpCsvService.java
+++ servicio-evidencias/src/main/java/com/galileo/cu/servicioevidencias/servicios/FtpCsvService.java
@@ -62,7 +62,7 @@ public class FtpCsvService {
 
         Page<String> listFiles = null;
         try {
-            listFiles = ListFiles(pageable);
+            listFiles = ListFiles(ftp, pageable);
         } catch (Exception e) {
             String err = "Error al obtener listado de ficheros .csv";
             log.error(err, e);
@@ -74,32 +74,12 @@ public class FtpCsvService {
         return listFiles;
     }
 
-    private Optional<Conexiones> obtenerConexionFTP() {
-        try {
-            return Optional.ofNullable(conRepo.findFirstByServicioContaining("ftp"));
-        } catch (Exception e) {
-            log.error("Error al consultar las conexiones FTP en la base de datos", e);
-            return Optional.empty();
-        }
-    }
-
     private Optional<Conexiones> getFTPConnection() {
         try {
             return Optional.ofNullable(conRepo.findFirstByServicioContaining("ftp"));
         } catch (Exception e) {
             log.error("Fallo al consultar las conexiones FTP en la base de datos", e);
             return Optional.empty();
-        }
-    }
-
-    private void conectarFTP(Conexiones con) throws IOException {
-        if (ProgEvidens.ftpCSV == null || !ProgEvidens.ftpCSV.isConnected()) {
-            try {
-                ProgEvidens.ftpCSV = crearConexionFTP(con);
-            } catch (Exception e) {
-                log.error("Error al conectar con el servidor FTP", e);
-                throw new IOException("Error al conectar con el servidor FTP: " + e.getMessage(), e);
-            }
         }
     }
 
@@ -136,9 +116,9 @@ public class FtpCsvService {
 
         int reply = ftp.getReplyCode();
         if (!FTPReply.isPositiveCompletion(reply)) {
-            String err = "Conexión fallida con el servidor FTP " + con.getIpServicio();
+            String err = "Fallo intentando conectar con el servidor FTP " + con.getIpServicio();
             log.error(err);
-            desconectarFTP(ftp);
+            disconnectFTP(ftp);
             throw new IOException(err);
         }
 
@@ -204,74 +184,9 @@ public class FtpCsvService {
         }
     }
 
-    private String obtenerDirectorioFTP() throws IOException {
-        try {
-            return ProgEvidens.ftpCSV.printWorkingDirectory();
-        } catch (Exception e) {
-            String err = "Error al obtener el directorio predeterminado del servidor FTP";
-            log.error(err, e);
-            desconectarFTP(ProgEvidens.ftpCSV);
-            throw new IOException(err, e);
-        }
-    }
-
-    private FTPClient crearConexionFTP(Conexiones con) throws IOException {
-        FTPClient ftp = new FTPClient();
-        try {
-            int puerto = Strings.isNullOrEmpty(con.getPuerto()) ? DEFAULT_FTP_PORT : Integer.parseInt(con.getPuerto());
-            ftp.connect(con.getIpServicio(), puerto);
-        } catch (Exception e) {
-            log.error("Error al intentar conectarse al servidor FTP {}:{}", con.getIpServicio(), con.getPuerto(), e);
-            throw new IOException("Error al conectarse con el servidor FTP: " + con.getIpServicio(), e);
-        }
-
-        int reply = ftp.getReplyCode();
-        if (!FTPReply.isPositiveCompletion(reply)) {
-            String err = "Conexión fallida con el servidor FTP " + con.getIpServicio();
-            log.error(err);
-            desconectarFTP(ftp);
-            throw new IOException(err);
-        }
-
-        autenticarFTP(ftp, con);
-
-        configurarModoPasivo(ftp);
-
-        return ftp;
-    }
-
-    private void autenticarFTP(FTPClient ftp, Conexiones con) throws IOException {
-        try {
-            boolean successLogin = ftp.login(con.getUsuario(), con.getPassword());
-            if (!successLogin) {
-                String err = "Autenticación fallida con el servidor FTP, intentando listar .csv";
-                log.error("{}", err);
-                desconectarFTP(ftp);
-                throw new IOException(err);
-            }
-            log.info("Autenticación exitosa con el servidor FTP");
-        } catch (IOException e) {
-            String err = "Error al autenticar con el servidor FTP " + con.getIpServicio() + ", intentando listar .csv";
-            log.error(err, e);
-            desconectarFTP(ftp);
-            throw new IOException(err, e);
-        }
-    }
-
-    private void configurarModoPasivo(FTPClient ftp) throws IOException {
-        try {
-            ftp.enterLocalPassiveMode();
-            ftp.setControlKeepAliveTimeout(1000);
-        } catch (Exception e) {
-            log.error("Error al configurar el modo pasivo en la conexión FTP", e);
-            desconectarFTP(ftp);
-            throw new IOException("Error al configurar el modo pasivo", e);
-        }
-    }
-
-    public Page<String> ListFiles(Pageable pageable) throws IOException {
-        FTPFile[] files = ProgEvidens.ftpCSV.listFiles();
-        // FTPFile[] dir = ProgEvidens.ftpCSV.listDirectories();
+    public Page<String> ListFiles(FTPClient ftp, Pageable pageable) throws IOException {
+        FTPFile[] files = ftp.listFiles();
+        // FTPFile[] dir = ftp.listDirectories();
         List<String> csvFiles = Arrays.stream(files)
                 .filter(file -> file.getName().toLowerCase().endsWith(".csv"))
                 .map(FTPFile::getName)
@@ -284,35 +199,43 @@ public class FtpCsvService {
     }
 
     public byte[] downloadFile(String fileName) throws IOException {
-        FTPClient ftpClient = new FTPClient();
+        String baseDir = DEFAULT_DIRECTORY;
+
+        Conexiones con = getFTPConnection()
+                .orElseThrow(() -> new IOException("No existe un servicio FTP entre las conexiones"));
+
+        FTPClient ftp = connectFTP(con, null);
+
+        if (!Strings.isNullOrEmpty(con.getRuta())) {
+            baseDir = con.getRuta();
+        } else {
+            baseDir = getFTPDirectory(ftp);
+        }
+        log.info(baseDir);
+
+        try {
+            ftp.changeWorkingDirectory(baseDir);
+        } catch (Exception e) {
+            String err = "Fallo al intentar cambiar al directorio {}";
+            log.error(err, baseDir, e);
+            disconnectFTP(ftp);
+            throw new IOException(err);
+        }
+
         try {
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            boolean success = ProgEvidens.ftpCSV.retrieveFile(fileName, outputStream);
+            boolean success = ftp.retrieveFile(fileName, outputStream);
 
             if (success) {
                 return outputStream.toByteArray();
             } else {
                 String err = "Fallo descargando el fichero: " + fileName;
                 log.error(err);
+                disconnectFTP(ftp);
                 throw new IOException(err);
             }
         } finally {
-            if (ProgEvidens.ftpCSV.isConnected()) {
-                ProgEvidens.ftpCSV.disconnect();
-            }
-        }
-    }
-
-    private void desconectarFTP(FTPClient ftp) throws IOException {
-        if (ftp != null && ftp.isConnected()) {
-            try {
-                ftp.disconnect();
-                ftp = null;
-                ProgEvidens.ftpCSV = null;
-            } catch (IOException e) {
-                log.error("Error al desconectar el servidor FTP", e);
-                throw e;
-            }
+            disconnectFTP(ftp);
         }
     }
 }
