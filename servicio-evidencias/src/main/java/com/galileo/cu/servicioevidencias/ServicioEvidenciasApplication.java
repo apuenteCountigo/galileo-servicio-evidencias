88a5887e854e9c7b576f7bcaa9befa4f64aa8179
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-19 11:01:35 -0400

    1- Deploy include docker save image

--- servicio-evidencias/src/main/java/com/galileo/cu/servicioevidencias/ServicioEvidenciasApplication.java
+++ servicio-evidencias/src/main/java/com/galileo/cu/servicioevidencias/ServicioEvidenciasApplication.java
@@ -20,7 +20,7 @@ public class ServicioEvidenciasApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("EVIDENCIAS V1.1-24-10-19 10:52");
+		System.out.println("EVIDENCIAS V1.1-24-10-19 11:01");
 	}
 
 }
