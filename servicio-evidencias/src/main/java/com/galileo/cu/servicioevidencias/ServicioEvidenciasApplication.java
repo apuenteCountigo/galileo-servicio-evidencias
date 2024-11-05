ead7ceb1e868faa4e7d40952fa28b699144e93ef
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-11-05 16:58:10 -0500

    mapAddress Recompilación

--- servicio-evidencias/src/main/java/com/galileo/cu/servicioevidencias/ServicioEvidenciasApplication.java
+++ servicio-evidencias/src/main/java/com/galileo/cu/servicioevidencias/ServicioEvidenciasApplication.java
@@ -20,7 +20,7 @@ public class ServicioEvidenciasApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("EVIDENCIAS V1.1.1 24-10-30 16:46");
+		System.out.println("EVIDENCIAS V1.1.2 24-11-05 16:56");
 	}
 
 }
