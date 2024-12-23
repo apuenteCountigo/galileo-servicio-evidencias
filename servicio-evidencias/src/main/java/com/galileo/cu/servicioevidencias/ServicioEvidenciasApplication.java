e62a7c07cb6de392704d8bdf0f29ef4fc5652566
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-12-23 06:34:56 -0500

    V1.1.4 FIX , SEPARATOR FOR CSV SEND 3000 PARAMETER

--- servicio-evidencias/src/main/java/com/galileo/cu/servicioevidencias/ServicioEvidenciasApplication.java
+++ servicio-evidencias/src/main/java/com/galileo/cu/servicioevidencias/ServicioEvidenciasApplication.java
@@ -20,7 +20,7 @@ public class ServicioEvidenciasApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("EVIDENCIAS V1.1.4 24-12-23 06:31");
+		System.out.println("EVIDENCIAS V1.1.4 24-12-23 06:35");
 	}
 
 }
