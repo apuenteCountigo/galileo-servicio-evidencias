d2234fd73ffe25e86e7cc0b8620bc7a041c743d7
apuentes+1-at-178934116267 <apuente@soaint.com>
2025-01-17 11:07:28 -0500

    V-1.1.5

--- servicio-evidencias/src/main/java/com/galileo/cu/servicioevidencias/ServicioEvidenciasApplication.java
+++ servicio-evidencias/src/main/java/com/galileo/cu/servicioevidencias/ServicioEvidenciasApplication.java
@@ -20,7 +20,7 @@ public class ServicioEvidenciasApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("EVIDENCIAS V1.1.4 24-12-23 06:35");
+		System.out.println("EVIDENCIAS V1.1.5 25-01-17 11:07");
 	}
 
 }
