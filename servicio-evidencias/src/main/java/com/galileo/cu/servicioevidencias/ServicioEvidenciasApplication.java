3336fccfcc0a3837bd4411adf2bfc751ddd1979e
apuentes+1-at-178934116267 <apuente@soaint.com>
2025-02-12 14:21:02 -0500

    versión 1.1.6 fix csv columna cellid topinimia

--- servicio-evidencias/src/main/java/com/galileo/cu/servicioevidencias/ServicioEvidenciasApplication.java
+++ servicio-evidencias/src/main/java/com/galileo/cu/servicioevidencias/ServicioEvidenciasApplication.java
@@ -20,7 +20,7 @@ public class ServicioEvidenciasApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("EVIDENCIAS V1.1.6 25-02-12 14:05");
+		System.out.println("EVIDENCIAS V1.1.6 25-02-12 14:20");
 	}
 
 }
